b5483bea8f97afe971810d3148c54df115d621a7
Chiel Douwes <chieldouwes@gmail.com>
2025-06-07 21:05:21 +0200

    refactor: add MPL headers to files that are missing the header

--- src/data/compression/prefix_search/mod.rs
+++ src/data/compression/prefix_search/mod.rs
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+// This Source Code Form is subject to the terms of the Mozilla Public         /
+// License, v. 2.0. If a copy of the MPL was not distributed with this         /
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.                   /
+//                                                                             /
+////////////////////////////////////////////////////////////////////////////////
+
 pub(crate) mod hash_chain;
 pub(crate) mod hash_table;
 pub(crate) mod multi_level_hash_chain;
