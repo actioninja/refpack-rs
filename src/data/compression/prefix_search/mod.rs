928c0113f2b9504f0d7aa900ee1a547762df4efb
Chiel Douwes <chieldouwes@gmail.com>
2025-06-09 01:03:52 +0200

    doc: document PrefixSearcher search function

--- src/data/compression/prefix_search/mod.rs
+++ src/data/compression/prefix_search/mod.rs
@@ -16,6 +16,15 @@ pub(crate) fn prefix(input_buf: &[u8]) -> [u8; 3] {
 
 pub(crate) trait PrefixSearcher<'a> {
     fn build(buffer: &'a [u8]) -> Self;
+
+    /// Search for all increasingly large matches in the search buffer.
+    ///
+    /// It is required to call search in order for all elements.
+    ///
+    /// This function will call found_fn with the position and range of match lengths for that position.
+    /// The function must return all matches with the search position `pos` starting from the closest
+    /// found position that are within [LONG_OFFSET_MAX](crate::data::control::LONG_OFFSET_MAX) bytes.
+    /// All returned matches should have a longer match length than the last.
     fn search<F: FnMut(usize, usize, usize)>(&mut self, pos: usize, found_fn: F);
 }
 
